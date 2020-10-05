a9d1756d8a7b3a4e0f7eee66167ddeb7a796b03d
DixiBoot <72413679+DixiBoot@users.noreply.github.com>
2020-10-06 00:43:02 +0300

    Update archi.sh

--- archi.sh
+++ archi.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+pacman -Syu
+
 read -p "Computer Name: " hostname
 read -p "User Name: " username
 
-echo 'Set Up Computer Name and Time Zone'
+echo "Set Up Computer Name and Time Zone"
 echo $hostname > /etc/hostname
 ln -svf /usr/share/zoneinfo/Europe/Minsk /etc/localtime
