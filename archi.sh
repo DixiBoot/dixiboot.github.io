3e1782e60cab0855f30575810011217dbbe104b5
DixiBoot <72413679+DixiBoot@users.noreply.github.com>
2020-10-06 19:23:57 +0300

    Update archi.sh

--- archi.sh
+++ archi.sh
@@ -15,18 +15,17 @@ pacstrap /mnt base base-devel linux linux-firmware
 genfstab -pU /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 
-exit
 
 ln -sf /usr/share/zoneinfo/Europe/Minsk /etc/localtime
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
 locale-gen
+echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
 
 hwclock --systohc --utc
 
 pacman -S grub --noconfirm
 
-echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
 echo '[multilib]' >> /etc/pacman.conf
 echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
 pacman -Syy
